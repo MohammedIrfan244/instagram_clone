c0747353ab874a51e84d016c05228cc0022b6879
user-irfan <kuttappuirfan244@gmail.com>
2024-12-27 15:07:00 +0530

    add Navbar component and integrate it into AppRoutes; refactor HomePage layout

--- client/src/pages/AppRoutes.tsx
+++ client/src/pages/AppRoutes.tsx
@@ -5,16 +5,20 @@ import LoginPage from './LoginPage'
 import { useSelector } from 'react-redux'
 import {RootState} from '../redux/store.ts'
 import HomePage from './HomePage'
+import Navbar from '../components/Navbar.tsx'
 
 function AppRoutes(): JSX.Element {
   const {currentUser} = useSelector((state: RootState) => state.currentUser)
   return (
+    <>
+    <Navbar/>
     <Routes>
       <Route path="/" element={<LandingPage />} /> 
       <Route path="/user/register" element={<RegisterPage />} />
       <Route path="/user/login" element={<LoginPage />} />
       <Route path="/home" element={currentUser ? <HomePage/> : <LoginPage />} />
     </Routes>
+    </>
   )
 }
 
